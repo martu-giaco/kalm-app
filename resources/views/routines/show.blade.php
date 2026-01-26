90ecba41d69970a522fb55d0e1bb55f1ac000d41
martu-giaco <martina.giacomini@davinci.edu.ar>
2026-01-26 00:56:10 -0300

    ya dejo de romper baii

--- resources/views/routines/show.blade.php
+++ resources/views/routines/show.blade.php
@@ -121,12 +121,6 @@
                     </svg>
                 </button>
             </form>
-
-            <a href="{{ route('routines.edit', $routine) }}" class="bg-[#3FADB3FF] hover:bg-[#254a4c] text-white h-14 w-14 rounded-full flex items-center justify-center shadow-xl transition transform hover:-translate-y-0.5">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="#fff" viewBox="0 0 24 24">
-                    <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a1.003 1.003 0 0 0 0-1.41l-2.34-2.34a1.003 1.003 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
-                </svg>
-            </a>
         </div>
     </section>
 </x-layout>
