fedb7447c3b266ac67a15af958c1182b39946f1b
martu-giaco <martina.giacomini@davinci.edu.ar>
2026-07-27 01:40:09 -0300

    ya está el modo oscuro de todas las pantallas de usuario???creoo???? falta el pete del admin

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -1,10 +1,10 @@
 <x-layout  title="Sobre Kälm">
-    <section class="px-5 pt-10 h-full rounded-t-3xl bg-white">
-        <h1 class="text-2xl font-semibold text-[#306067] mb-3">Sobre Kälm</h1>
+    <section class="px-5 pt-10 h-full rounded-t-3xl bg-white dark:bg-[#2A4043]">
+        <h1 class="text-2xl font-semibold text-[#306067] dark:text-[#CCE2E5] mb-3">Sobre Kälm</h1>
         <div>
-            <p class="mb-7">En el equipo de <i>Kälm</i> nos enfocamos en brindar apoyo y recursos para el bienestar integral de las personas de manera gratuita, promoviendo el amor propio, la paciencia y la autocompasión.</p>
-            <p class="mb-7">Creemos que el bienestar es un viaje personal, único y escencial para todos. Nuestro propósito es acompañarte con cuidado, transparencia y amor, ofreciéndote herramientas gratuitas que te ayuden a reconectar con vos mism@ y con lo que realmente necesitás. Cada rutina, cada recordatorio, cada momento de pausa es una invitación a volver a vos. Porque el autocuidado no se trata de perfección, sino de aprender a mirarte con paciencia, a cuidarte con ternura y a celebrar cada pequeño paso en tu propio ritmo.</p>
-            <p>Con la frescura de lo simple y la sinceridad de lo real, en <i>Kälm</i> te recordamos que el bienestar no es una meta, sino un viaje único, personal y esencial, y estamos acá para acompañarte en cada paso del camino.</p>
+            <p class="mb-7 text-[#306067] dark:text-[#E9E5E3]">En el equipo de <i>Kälm</i> nos enfocamos en brindar apoyo y recursos para el bienestar integral de las personas de manera gratuita, promoviendo el amor propio, la paciencia y la autocompasión.</p>
+            <p class="mb-7 text-[#306067] dark:text-[#E9E5E3]">Creemos que el bienestar es un viaje personal, único y escencial para todos. Nuestro propósito es acompañarte con cuidado, transparencia y amor, ofreciéndote herramientas gratuitas que te ayuden a reconectar con vos mism@ y con lo que realmente necesitás. Cada rutina, cada recordatorio, cada momento de pausa es una invitación a volver a vos. Porque el autocuidado no se trata de perfección, sino de aprender a mirarte con paciencia, a cuidarte con ternura y a celebrar cada pequeño paso en tu propio ritmo.</p>
+            <p class="text-[#306067] dark:text-[#E9E5E3]">Con la frescura de lo simple y la sinceridad de lo real, en <i>Kälm</i> te recordamos que el bienestar no es una meta, sino un viaje único, personal y esencial, y estamos acá para acompañarte en cada paso del camino.</p>
         </div>
     </section>
 </x-layout>
